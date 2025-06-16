4314dfe1122e7d0e39d45b1a69f4a25ee13b8746
Trinity Stallworth <146296799+Trinity5757@users.noreply.github.com>
2025-06-16 13:15:06 -0700

    Time: 2104 ms (20.85%) | Memory: 40.7 MB (31.22%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         
-        ArrayList<Integer> divisors = new ArrayList<>();
         int count = num - 1;
         int amount = 0;
 
@@ -9,19 +8,11 @@ class Solution {
         {
             if(num % count == 0)
             {
-                divisors.add(count);
+                amount += count;
             }
             count--;
         }
 
-        for(int i : divisors)
-        {
-            amount += i;
-        }
-        if(amount == num)
-        {
-            return true;
-        }        
-        return false;
+        return amount == num;
     }
 }
