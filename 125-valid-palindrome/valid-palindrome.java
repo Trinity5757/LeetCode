6e5c8b2314ac8acf8f6b670ca6797e1ab5a1781d
Trinity Stallworth <146296799+Trinity5757@users.noreply.github.com>
2025-06-21 18:40:09 -0700

    Time: 19 ms (11.92%) | Memory: 45.2 MB (23.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -6,7 +6,7 @@ class Solution {
         s = s.toLowerCase();
         int j = s.length() - 1;
         System.out.println(s);
-        for(int i = 0; i < j; i++)
+        for(int i = 0; i < s.length() / 2; i++)
         {
             if(s.charAt(i) != s.charAt(j - i)) return false;
         }
